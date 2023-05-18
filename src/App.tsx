f94f204fa90283c9a005477f5ee73b5bbb1e0679
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-05-17 17:10:17 -0700

    Task 3.2 - add Counter component with redux (without using folders 'actions' and 'reducers')

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import CounterClassComponent from './components/CounterClassComponent'
 import CounterFunctionalComponent from './components/CounterFunctionalComponent'
+import Counter from './features/counter/Counter'
 
 function App() {
   return (
     <>
       <CounterFunctionalComponent initialCount={1000} />
       <CounterClassComponent initialCount={2000} />
+      <Counter />
     </>
   )
 }
---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,18 @@
+import { increment } from './counterSlice'
+import { useAppSelector, useAppDispatch } from '../../app/hooks'
+
+function Counter() {
+    const count = useAppSelector(state => state.counter.value)
+    const dispatch = useAppDispatch()
+
+    return (
+        <div>
+            <h2>Counter Component With Redux</h2>
+            <button onClick={() => dispatch(increment())}>Increment count</button>
+            <div>Count is: {count}</div>
+        </div>
+    )
+
+}
+
+export default Counter

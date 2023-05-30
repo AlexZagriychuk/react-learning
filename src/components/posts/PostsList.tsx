ef8c0de1841d9f8890bcffaf2e264422c14aeabc
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-05-30 12:54:07 -0700

    Task 3.3 - removing Redux state normalization for Posts

--- src/components/posts/PostsList.tsx
+++ src/components/posts/PostsList.tsx
@@ -1,21 +1,29 @@
 import { useAppSelector } from "../../redux/hooks"
-import { selectPostsNormalized } from "../../redux/modules/postsSlice"
+import { selectPosts } from "../../redux/modules/postsSlice"
 import { selectAllUsersNormalized } from "../../redux/modules/usersSlice"
 import "./PostsList.css"
 import PostsListItem from "./PostsListItem"
+import { Post } from "./posts"
 
 export default function PostsList() {
-    const postsNormalized = useAppSelector(selectPostsNormalized)
+    const posts = useAppSelector(selectPosts)
     const usersNormalized = useAppSelector(selectAllUsersNormalized)
+
+    const getPostsListItemsReversed = (posts: Post[]) => {
+        const res  = [] 
+        for(let index = posts.length - 1; index >= 0; index--) {
+            let post = posts[index]
+            res.push(<PostsListItem key={post.id} post={post} user={usersNormalized[post.userId]}/>)
+        }
+        return res 
+    }
 
     return (
         <>
             <h2>Posts:</h2>
 
             <ul className="posts-list">
-                {Object.values(postsNormalized).reverse().map(post => {
-                    return <PostsListItem key={post.id} post={post} user={usersNormalized[post.userId]}/>
-                })}
+                {getPostsListItemsReversed(posts)}
             </ul>
         </>
     )
--- src/redux/modules/postsSlice.ts
+++ src/redux/modules/postsSlice.ts
@@ -1,33 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Post, getAllPosts } from "../../components/posts/posts";
+import { getAllPosts } from "../../components/posts/posts";
 import { RootState } from "../store";
 
-export interface PostsNormalized {
-    [key: number]: Post
-}
-
-export interface PostsState {
-    nextPostId: number,
-    postsNormalized: PostsNormalized
-}
-
-const generateInitialState = () : PostsState => {
-    const allPosts = getAllPosts()
-
-    const postsNormalized = allPosts.postsData.reduce((acc, post) => {
-        acc[post.id] = post
-        return acc
-    }, {} as PostsNormalized)
-
-    return {
-        nextPostId: allPosts.nextPostId,
-        postsNormalized
-    }
-}
 
 export const postsSlice = createSlice({
     name: "posts",
-    initialState: generateInitialState(),
+    initialState: getAllPosts(),
     reducers: {
         postAdded: function() {
             // Not implemented
@@ -36,7 +14,7 @@ export const postsSlice = createSlice({
 })
 
 export const selectNextPostId = (state: RootState) => state.posts.nextPostId
-export const selectPostsNormalized = (state: RootState) => state.posts.postsNormalized
+export const selectPosts = (state: RootState) => state.posts.postsData
 
 export const { postAdded } = postsSlice.actions
 export default postsSlice.reducer

3ccf97f1ff921e4653c056164b53f9fd061bc8c3
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-05-30 12:27:16 -0700

    Task 3.3 - extracting date functions into the DateUtils file

--- src/components/posts/PostsListItem.tsx
+++ src/components/posts/PostsListItem.tsx
@@ -1,32 +1,11 @@
 import { useAppSelector } from "../../redux/hooks";
 import { selectCurrentUserId } from "../../redux/modules/usersSlice";
+import { dateDiffAsString } from "../../utils/DateUtils";
 import { User } from "../user/users";
 import { Post } from "./posts";
 
 export default function PostsListItem(props: {post: Post, user: User}) {
     const currentUserId = useAppSelector(selectCurrentUserId)
-
-    const getDateStrEnding = (dateMemberValue: number) => {
-        return dateMemberValue === 1 ? "" : "s"
-    }
-
-    const dateDiffAsString = (date1: Date, date2 : Date = new Date()) => {
-        const diffInMinutesTotal = Math.abs(Math.floor((date2.getTime() - date1.getTime()) / 60000))
-        const diffInHoursTotal = Math.floor(diffInMinutesTotal / 60)
-        const diffInDaysTotal = Math.floor(diffInHoursTotal / 24)
-
-        if(diffInMinutesTotal < 60) {
-            return `${diffInMinutesTotal} minute${getDateStrEnding(diffInMinutesTotal)}`
-        }
-
-        if(diffInHoursTotal < 24) {
-            const minutes = Math.floor(diffInMinutesTotal - diffInHoursTotal * 60)
-            return `${diffInHoursTotal} hour${getDateStrEnding(diffInHoursTotal)}, ${minutes} minute${getDateStrEnding(minutes)}`
-        }
-
-        const hours = Math.floor(diffInHoursTotal - diffInDaysTotal * 24)
-        return `${diffInDaysTotal} day${getDateStrEnding(diffInDaysTotal)}, ${hours} hour${getDateStrEnding(hours)}`
-    }
 
     return (
         <li className={currentUserId === props.post.userId ? "posted-by-current-user" : ""}>

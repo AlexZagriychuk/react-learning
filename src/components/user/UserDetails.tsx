c1c06c6d6343c2ea6e0c0f3298320eb9a0a15ce3
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-06-17 15:00:09 -0700

    Task 3.7 - User Details page (without icons)

--- src/components/user/UserDetails.tsx
+++ src/components/user/UserDetails.tsx
@@ -1,10 +1,49 @@
 import { useParams } from "react-router-dom";
+import { useAppSelector } from "../../redux/hooks";
+import { selectUserById } from "../../redux/modules/usersSlice";
+import { EntityId } from "@reduxjs/toolkit";
+import "./UserDetails.css"
 
 export default function UserDetails() {
     const params = useParams();
     const userId = params.userId
-    
+
+    const user = useAppSelector(state => selectUserById(state, userId as EntityId))
+    let userDetailsFields = [] as Array<{iconClass: string, label: string, value: string}>
+    if(user) {
+        const userAddress = `${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}`
+        userDetailsFields = [
+            {iconClass: "", label: "Name", value: user.name},
+            {iconClass: "", label: "User Name", value: user.username},
+            {iconClass: "", label: "Email", value: user.email},
+            {iconClass: "", label: "Phone", value: user.phone},
+            {iconClass: "", label: "Website", value: user.website},
+            {iconClass: "", label: "Address", value: userAddress},
+            {iconClass: "", label: "Company", value: user.company.name},
+        ]    
+    }
+
     return (
-        <h2>User Details {userId}:</h2>
+        <>
+            <h2>User Details:</h2>
+            {user === undefined
+                ? <p>Loading...</p>
+                : <div className="user-details">
+                    <img className="user-details-avatar" src={user.avatarBig} alt="" />
+                    <ul className="user-details-list">
+                        {userDetailsFields.map(userDetailsField => {
+                            return <li className="user-details-list-item" key={userDetailsField.label}>
+                                <i className={"user-details-icon" + userDetailsField.iconClass}>x</i>
+                                <div className="user-details-data">
+                                    <span className="user-details-data-value">{userDetailsField.value}</span>
+                                    <span className="user-details-data-label">{userDetailsField.label}</span>
+                                </div>
+                            </li>
+                        })}
+                    </ul>
+                    <label htmlFor=""></label>
+                </div>
+            }
+        </>
     )
 }

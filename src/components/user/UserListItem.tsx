fb7ff655115f76affbeee734bb9dda1849150417
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-06-20 11:21:13 -0700

    Task 3.7 - using Link for navigation to UserDetails for accessibility

--- src/components/user/UserListItem.tsx
+++ src/components/user/UserListItem.tsx
@@ -1,18 +1,18 @@
 import { User } from "./users";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function UserListItem({id, avatarSmall: avatar, name, username, type, registered}: User ) {
-    const navigate = useNavigate();
-    
     return (
-        <li className="user" onClick={() => navigate(`./${id}`)}>
-            <img className="user-avatar" src={avatar} alt="" />
-            <div className="user-info">
-                <span className="user-name"><b>UserName: </b>{username}</span>
-                <span className="user-name"><b>Name: </b>{name}</span>
-                <span className="user-type"><b>Type: </b>{type}</span>
-                <span className="user-registered"><b>Registered: </b>{registered}</span>
-            </div>
+        <li>
+            <Link className="user" to={`./${id}`}>
+                <img className="user-avatar" src={avatar} alt="" />
+                <div className="user-info">
+                    <span className="user-name"><b>UserName: </b>{username}</span>
+                    <span className="user-name"><b>Name: </b>{name}</span>
+                    <span className="user-type"><b>Type: </b>{type}</span>
+                    <span className="user-registered"><b>Registered: </b>{registered}</span>
+                </div>
+            </Link>
         </li>
     )
 }

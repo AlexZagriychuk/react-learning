80a13870ed2caaa21ee0a30ce2c463d40324d2ac
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-06-20 19:37:51 -0700

    Task 3.8 - add user details tabs into UserDetails

--- src/components/user/user_details/UserDetails.tsx
+++ src/components/user/user_details/UserDetails.tsx
@@ -1,14 +1,34 @@
 import { useParams } from "react-router-dom";
 import "./UserDetails.css"
+import { useState } from "react";
 import UserDetailsInfo from "./UserDetailsInfo";
 
 export default function UserDetails() {
     const params = useParams();
     const userId = parseInt(params.userId as string)
 
+    enum UserDetailsTab {
+        "Info",
+        "Albums",
+        "ToDo",
+        "Posts"
+    }
+
+    const [activeTab, setActiveTab] = useState(UserDetailsTab.Info)
+
     return (
         <>
             <h2>User Details:</h2>
+
+            <ul className="user-details-tabs">
+                {Object.entries(UserDetailsTab).filter(userDetailsTab => isNaN(parseInt(userDetailsTab[0]))).map(tabEntry => 
+                    <li 
+                        key={tabEntry[0]}
+                        className={activeTab === tabEntry[1] as UserDetailsTab ? "active" : ""}
+                        onClick={() => setActiveTab(tabEntry[1] as UserDetailsTab)}
+                    >{tabEntry[0]}</li>
+                )}
+            </ul>
 
             <div className="user-details-content">
                 <UserDetailsInfo userId={userId} />

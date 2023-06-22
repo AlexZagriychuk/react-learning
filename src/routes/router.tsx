06acbc2764fd9a86cb30133e7aadb5e513a6a51e
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-06-22 12:58:54 -0700

    Refactoring - extracting router from main.tsx

---
+++ src/routes/router.tsx
@@ -0,0 +1,40 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Root from "../components/root/Root";
+import ErrorPage from "../components/error/ErrorPage";
+import PostsList from "../components/posts/PostsList";
+import ToDoList from "../components/todo/ToDoList";
+import UserList from "../components/user/UserList";
+import UserDetails from "../components/user/user_details/UserDetails";
+
+export const router = createBrowserRouter(
+    [
+        {
+            path: '',
+            element: <Navigate to='/posts' />,
+        },
+        {
+            path: '/',
+            element: <Root />,
+            errorElement: <ErrorPage />,
+            children: [
+                {
+                    path: 'posts',
+                    element: <PostsList />,
+                },
+                {
+                    path: 'todo',
+                    element: <ToDoList />,
+                },
+                {
+                    path: 'users',
+                    element: <UserList />,
+                },
+                {
+                    path: 'users/:userId',
+                    element: <UserDetails />,
+                }
+            ],
+        },
+    ],
+    { basename: '/react' }
+);

d3770a4d099cc22af6c678f5066afb6acd8b2858
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-08-07 23:38:51 -0700

    Change the backend URL

--- src/redux/modules/common_api/baseApi.ts
+++ src/redux/modules/common_api/baseApi.ts
@@ -8,6 +8,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 // })
 export const baseApiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder-clone.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://node-jsonplaceholder-clone.fly.dev' }),
     endpoints: () => ({})
 })
